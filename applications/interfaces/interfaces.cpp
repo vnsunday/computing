5927503323319f17e5478011de4fe3976cab57cc
vnsunday <truyenspidermanvn@gmail.com>
2026-08-16 15:59:23 +0700

    interfaces

---
+++ applications/interfaces/interfaces.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void UniversalSerialBus() {
+    char type; // 
+    char VBUS; // 
+    char Dplus;
+    char Dminus;
+    char GRND; // Ground
+
+    char pins[4]; // Contacts of the connector
+}
+
+void UniversalSerialBus_C() {
+    const char type = 'C'; //   
+    char pins[24];
+    char VBUS;
+    char GND;
+    char Dplus;
+    char Dminus;
+}
+
+int main()
+{
+    return 0;
+}
